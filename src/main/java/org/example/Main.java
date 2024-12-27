805789e08a33dd62b0ac8596298c67684625cc9c
lukalomtadze <lomtadze418@gmail.com>
2024-12-27 20:16:47 +0400

    example of arithmetical operations code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,18 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-
+//        Evaluating evaluating = new Evaluating();
+//
+//        String code = """
+//                x = 6
+//                x = x + 1
+//                y = x + 2
+//                print(x)
+//                print(y)
+//                x = y + x
+//                print(x)
+//                """;
+//
+//        evaluating.eval(code);
     }
 }
