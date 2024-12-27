716549511726ea605859542bf0c2678ee9afa6ac
lukalomtadze <lomtadze418@gmail.com>
2024-12-28 00:52:26 +0400

    added for loop using regular expresion pattern regonition and made some cahnges in evaluation class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,18 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        Evaluating evaluating = new Evaluating();
 
+        // Python-like code for summing the first N natural numbers using range
+        String code = """
+                x = 1
+                y = 50
+                
+                for i in range(1, y):
+                    x = x + i
+                print(x)
+                """;
+
+        evaluating.eval(code);
     }
 }
