e84a16bdd2f13fa8a4d0a495e50ee0d2eee669e7
lukalomtadze <lomtadze418@gmail.com>
2024-12-27 20:06:27 +0400

    removed python code before merging

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,6 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Evaluating interpreter = new Evaluating();
 
-        String code = """
-                x = 6 + 2 * 5
-                print(x)
-                
-                y = 6
-                x = y
-                print(x)
-                """;
-        interpreter.eval(code);
     }
 }
