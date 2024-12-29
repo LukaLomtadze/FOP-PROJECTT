b60101a55b79cfc9efddbccf13d7ef369557ba1f
lukalomtadze <lomtadze418@gmail.com>
2024-12-29 21:26:02 +0400

    ....

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Evaluating evaluating = new Evaluating();
 
-        String code = """
-                
-                """;
 
-        evaluating.eval(code);
     }
 }
